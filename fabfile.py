fc212853b16cdfdb8bf1250b22f44e49490d2cc1
Mark Striemer <mstriemer@gmail.com>
2011-07-19 19:33:09 -0500

    Adds 404 and 500 templates and fabfile
    Adds the templates for 404 and 500 errors.
    Adds a fabfile for deployment.
    Updates how static files are served in development mode.

---
+++ fabfile.py
@@ -0,0 +1,47 @@
+from __future__ import print_function
+from datetime import datetime
+
+from fabric.api import local, run, env, cd, sudo
+
+env.hosts = ['deploy@ubuntu']
+
+def archive(branch='master'):
+    with cd("~/bigbrother"):
+        run("git pull")
+        run("git archive --format zip --output archive.zip {0}".format(branch))
+
+def expand(target='/var/apps/bigbrother/releases/'):
+    folder = datetime.now().strftime('%Y%m%d%H%M%S')
+    sudo("mkdir -p {0}{1}/bigbrother".format(target, folder))
+    sudo("unzip ~/bigbrother/archive.zip -d {0}{1}/bigbrother".format(target,
+        folder))
+    run("rm ~/bigbrother/archive.zip")
+    hack_local_files(target, folder)
+    update_symlinks(target, folder)
+
+def hack_local_files(target, folder):
+    current = target + folder + '/bigbrother'
+    sudo("mv {0}/settings.py {0}/settings.py.orig".format(current))
+    sudo("cp {0}current/bigbrother/settings.py {1}/settings.py".format(
+        target, current))
+
+def update_symlinks(target, folder):
+    current = target + folder
+    previous = target + 'current'
+    sudo("rm {0}".format(previous))
+    sudo("ln -s {0} {1}".format(current, previous))
+
+
+def reload_apache():
+    sudo("/etc/init.d/apache2 reload")
+
+def deploy(branch=None, expand_target=None):
+    if branch is None:
+        archive()
+    else:
+        archive(branch)
+    if expand_target is None:
+        expand()
+    else:
+        expand(expand_target)
+    reload_apache()
--- urls.py
+++ urls.py
@@ -9,14 +9,19 @@ admin.autodiscover()
 urlpatterns = patterns('',
     (r'^$', redirect_to, {'url': '/bigbrother/'}),
     (r'^bigbrother/', include('bigbrother.gameshow.urls')),
-    (r'^databrowse/(.*)', databrowse.site.root),
     (r'^admin/doc/', include('django.contrib.admindocs.urls')),
     (r'^admin/', include(admin.site.urls)),
-    (r'^static/(?P<path>.*)$', 'django.views.static.serve',
-        {'document_root': settings.MEDIA_ROOT}),
     (r'^accounts/login/$', 'django.contrib.auth.views.login'),
     (r'^accounts/logout/$', 'django.contrib.auth.views.logout_then_login'),
     (r'^accounts/password_change/$',
         'django.contrib.auth.views.password_change', {'post_change_redirect':
         '/bigbrother/'}),
 )
+
+if settings.DEBUG:
+    urlpatterns += patterns('',
+        url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {
+            'document_root': settings.MEDIA_ROOT,
+        }),
+   )
+
