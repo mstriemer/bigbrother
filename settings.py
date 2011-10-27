859ce1322484e0826c020ea2e2cb66d0b34c5856
Mark Striemer <mstriemer@gmail.com>
2011-10-27 08:29:50 -0500

    Adds requirements, fixes path for my air
    Adds the requirements.txt file.
    Updates paths in settings.py to be correct for my new macbook.

--- settings.py
+++ settings.py
@@ -45,7 +45,7 @@ USE_L10N = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/"
-MEDIA_ROOT = '/Users/Mark/git/bigbrother/media/'
+MEDIA_ROOT = '/Users/Mark/Projects/bigbrother/media/'
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash if there is a path component (optional in other cases).
@@ -76,7 +76,7 @@ MIDDLEWARE_CLASSES = (
 ROOT_URLCONF = 'bigbrother.urls'
 
 TEMPLATE_DIRS = (
-    '/Users/Mark/git/bigbrother/templates'
+    '/Users/Mark/Projects/bigbrother/templates'
 )
 
 INSTALLED_APPS = (
