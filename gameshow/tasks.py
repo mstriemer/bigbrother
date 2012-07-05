d630a626529f0521af40cc93377f49f93919d86e
Mark Striemer <mstriemer@gmail.com>
2012-07-04 23:42:31 -0500

    send reminders for the current day

--- gameshow/tasks.py
+++ gameshow/tasks.py
@@ -3,7 +3,7 @@ from django.core.mail import send_mass_mail
 from gameshow.models import Gameshow
 
 
-def send_email_reminders(predictions, users):
+def send_prediction_reminder_emails(predictions, users):
     if not predictions:
         return
     mails = []
@@ -18,26 +18,12 @@ def send_email_reminders(predictions, users):
                 message.append(u'    {i}. {choice}'.format(choice=choice, i=i))
             else:
                 message.append(u'    * You have not made a prediction')
+            message.append(u'')
         mails.append((subject, u'\n'.join(message), sender, [user.email]))
     send_mass_mail(mails)
 
-def old_send_email_reminders():
+def notify_users_of_todays_predictions():
     gameshow = Gameshow.objects.current()
-    preds = [p for e in gameshow.event_set.today()
-               for p in e.prediction_set.all()]
-    mails = []
-    if preds:
-        for user in gameshow.users.all():
-            body = ['You have the following predictions due today:', '']
-            for prediction in preds:
-                body.append("* {name}".format(name=prediction))
-                for up in prediction.userprediction_set.filter(user=user):
-                    for upc in up.userpredictionchoice_set.all():
-                        body.append("    * {choice}".format(choice=upc))
-                    else:
-                        body.append("    * You have not made a prediction")
-            mails.append(("Today's predictions",
-                          "\n".join(body),
-                          "bigbrother@striemer.ca",
-                          user.email))
-    return mails
+    predictions = gameshow.todays_predictions()
+    users = gameshow.users.all()
+    send_prediction_reminder_emails(predictions, users)
--- gameshow/tests.py
+++ gameshow/tests.py
@@ -1,12 +1,11 @@
 from django.core import mail
 from django.test import TestCase
 from django.contrib.auth.models import User
+from mock import Mock, patch
 
-from gameshow.models import Prediction, UserPredictionChoice
-from gameshow.tasks import send_email_reminders
-
-from mock import Mock
-
+from gameshow.models import Prediction, UserPredictionChoice, Gameshow
+from gameshow.tasks import (send_prediction_reminder_emails,
+        notify_users_of_todays_predictions)
 
 class SendEmailRemindersTest(TestCase):
     def setUp(self):
@@ -23,17 +22,17 @@ class SendEmailRemindersTest(TestCase):
             self.predictions.append(prediction)
 
     def test_users_are_not_emailed_about_nothing(self):
-        send_email_reminders([], self.users)
+        send_prediction_reminder_emails([], self.users)
         self.assertEqual(0, len(mail.outbox))
 
     def test_users_are_emailed_about_something(self):
-        send_email_reminders(self.predictions, self.users)
+        send_prediction_reminder_emails(self.predictions, self.users)
         self.assertEqual(2, len(mail.outbox))
         for i, user in enumerate(self.users):
             self.assertEqual([user.email], mail.outbox[i].recipients())
 
     def test_users_are_informed_of_predictions(self):
-        send_email_reminders(self.predictions, self.users)
+        send_prediction_reminder_emails(self.predictions, self.users)
         for title in self.titles:
             for email in mail.outbox:
                 self.assertEqual("[Big Brother] Today's Predictions",
@@ -41,13 +40,13 @@ class SendEmailRemindersTest(TestCase):
                 self.assertIn(title, email.body)
 
     def test_emails_are_sent_from_the_right_address(self):
-        send_email_reminders(self.predictions, self.users)
+        send_prediction_reminder_emails(self.predictions, self.users)
         for email in mail.outbox:
             self.assertEqual('bigbrother@striemer.ca', email.from_email)
 
     def test_users_are_informed_they_havent_chosen(self):
         self.predictions[0].user_choices = Mock(return_value=[])
-        send_email_reminders([self.predictions[0]], self.users)
+        send_prediction_reminder_emails([self.predictions[0]], self.users)
         for email in mail.outbox:
             self.assertIn('You have not made a prediction', email.body)
 
@@ -56,12 +55,37 @@ class SendEmailRemindersTest(TestCase):
         user_choice = Mock(spec_set=UserPredictionChoice)
         user_choice.__unicode__ = lambda self: name
         self.predictions[0].user_choices = Mock(return_value=[user_choice])
-        send_email_reminders([self.predictions[0]], [self.users[0]])
+        send_prediction_reminder_emails([self.predictions[0]], [self.users[0]])
         for email in mail.outbox:
             self.assertIn(name, email.body)
 
     def test_users_are_informed_of_how_many_choices_to_make(self):
         self.predictions[0].number_of_choices = 8128
-        send_email_reminders([self.predictions[0]], [self.users[0]])
+        send_prediction_reminder_emails([self.predictions[0]], [self.users[0]])
         for email in mail.outbox:
             self.assertIn(u'{} (pick 8128)'.format(self.titles[0]), email.body)
+
+
+@patch.object(Gameshow, 'objects')
+class TestNotifyUsersOfTodaysPredictions(TestCase):
+    def test_gameshow_is_found(self, gameshow_objects):
+        notify_users_of_todays_predictions()
+        gameshow_objects.current.assert_called_with()
+
+    def test_predictions_are_found(self, gameshow_objects):
+        gameshow = gameshow_objects.current()
+        notify_users_of_todays_predictions()
+        gameshow.todays_predictions.assert_called_with()
+
+    def test_users_are_found(self, gameshow_objects):
+        gameshow = gameshow_objects.current()
+        notify_users_of_todays_predictions()
+        gameshow.users.all.assert_called_with()
+
+    def test_reminders_are_sent(self, gameshow_objects):
+        gameshow = gameshow_objects.current()
+        predictions = gameshow.todays_predictions()
+        users = gameshow.users.all()
+        with patch('gameshow.tasks.send_prediction_reminder_emails') as send_emails:
+            notify_users_of_todays_predictions()
+            send_emails.assert_called_with(predictions, users)
