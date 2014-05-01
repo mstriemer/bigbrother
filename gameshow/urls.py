c510a73550c4c8895b2695ea99f635dab65bc44b
Mark Striemer <mstriemer@gmail.com>
2014-05-01 19:05:53 -0400

    initial API

--- gameshow/urls.py
+++ gameshow/urls.py
@@ -1,7 +1,32 @@
-from django.conf.urls.defaults import patterns
+from django.conf.urls.defaults import include, patterns
+from django.contrib.auth.models import User
+
+from rest_framework import viewsets, routers
+
+from gameshow.models import Contestant, Gameshow
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    model = User
+
+
+class GameshowViewSet(viewsets.ModelViewSet):
+    model = Gameshow
+    lookup_field = 'slug'
+    lookup_url_kwarg = 'gameshow_slug'
+
+
+class ContestantViewSet(viewsets.ModelViewSet):
+    model = Contestant
+
+router = routers.DefaultRouter()
+router.register('users', UserViewSet)
+router.register('gameshows', GameshowViewSet)
+router.register('contestants', ContestantViewSet)
 
 urlpatterns = patterns('',
     (r'^$', 'gameshow.views.redirect_to_current'),
+    (r'^', include(router.urls)),
     (r'^rules/$', 'gameshow.views.rules'),
     (r'^(?P<gameshow_slug>[a-z0-9-]+)/$', 'gameshow.views.dashboard'),
     (r'^(?P<gameshow_slug>[a-z0-9-]+)/predictions/(?P<pk>\d+)/$', 'gameshow.views.prediction_detail'),
