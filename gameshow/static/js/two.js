77ff5d6550c68adfe933417ec99615387a3722f5
Mark Striemer <mstriemer@gmail.com>
2014-06-05 08:18:05 -0500

    x-data-list component is a gross hack

--- gameshow/static/js/two.js
+++ gameshow/static/js/two.js
@@ -1,3 +1,35 @@
+xtag.register('x-data-list', {
+    lifecycle: {
+        created: function () {
+            this.rootElement = this.children[0];
+            this.template = this.rootElement.innerHTML;
+            this.clearData();
+            $.get(this.src).done(this.loadData.bind(this));
+        },
+    },
+    accessors: {
+        src: {attribute: 'src'},
+        root: {attribute: 'root'},
+    },
+    methods: {
+        loadData: function (data) {
+            var results = data[this.root];
+            var content;
+            results.forEach((function (result) {
+                content = this.template;
+                for (var attr in result) {
+                    content = content.replace(
+                        new RegExp("{" + attr + "}", 'g'), result[attr]);
+                }
+                this.rootElement.innerHTML += content;
+            }).bind(this));
+        },
+        clearData: function () {
+            this.children[0].innerHTML = '';
+        },
+    },
+});
+
 document.addEventListener('DOMComponentsLoaded', function () {
     var appBar = document.querySelector('x-appbar');
     var flipBox = document.querySelector('x-flipbox');
