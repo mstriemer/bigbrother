14376e7c0485b047203245a5e2f85783650565a6
Mark Striemer <mstriemer@gmail.com>
2015-04-07 19:39:53 -0500

    Big Brother Canada 3 schedule

--- gameshow/management/commands/create_events.py
+++ gameshow/management/commands/create_events.py
@@ -4,20 +4,19 @@ from datetime import date, timedelta
 
 from gameshow.tasks import create_events_for_date, event_schedule
 
+
 class Command(BaseCommand):
     help = 'Create events for the next show night.'
 
     def handle(self, day_to_create_events, *args, **options):
-        if date.today().strftime('%A') != day_to_create_events:
+        today = date.today()
+        if today.strftime('%A') != day_to_create_events:
             print("Not creating any events today.")
             return
         else:
             print("Today's the day, creating events.")
-        last_show = date.today()
-        one_day = timedelta(days=1)
         has_show = lambda day: day.strftime('%A') in event_schedule
-        next_week = [last_show + timedelta(days=i) for i in range(1, 8)]
+        next_week = [today + timedelta(days=i) for i in range(1, 8)]
         show_days = [day for day in next_week if has_show(day)]
-        for day in show_days:
-            create_events_for_date(day, due_date=last_show + one_day)
-            last_show = day
+        for day, last_show in zip(show_days, [today] + show_days):
+            create_events_for_date(day, due_date=last_show + timedelta(days=1))
